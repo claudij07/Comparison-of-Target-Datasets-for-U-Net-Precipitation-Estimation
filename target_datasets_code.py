b7c6b97921fe1a4fec2aa628a5c80bb67f29c3cc
claudij07 <97997024+claudij07@users.noreply.github.com>
2025-07-24 12:26:54 -0700

    Update target_datasets_code.py
    Updated code.

--- target_datasets_code.py
+++ target_datasets_code.py
@@ -23,51 +23,47 @@ with h5py.File(path+"IR_INPUT_VALIDATION.mat", 'r') as f:
 with h5py.File(path+"PRECIPITATION_TARGET_VALIDATION.mat", 'r') as f:
     validation_target = np.array(f["PRECIPITATION_TARGET_VALIDATION"])[..., np.newaxis]
 
-
 #Creating the convolution, encoder and decoder blocks:
-def conv_block(input, num_filters):
-    x = keras.layers.Conv2D(num_filters, 3, padding="same", activation='relu')(input)
-    x = keras.layers.Conv2D(num_filters, 3, padding="same", activation='relu')(x)
+def conv_block(input, num_filters, weight_decay=WEIGHT_DECAY_VALUE):
+    x = keras.layers.Conv2D(num_filters, 3, padding="same", activation='relu',
+                            kernel_regularizer=l2(weight_decay))(input)
+    x = keras.layers.Conv2D(num_filters, 3, padding="same", activation='relu',
+                            kernel_regularizer=l2(weight_decay))(x)
     return x
 
 def encoder_block(input, num_filters):
     x = conv_block(input, num_filters)
     p = keras.layers.MaxPooling2D((2,2))(x)
-    return x, p 
+    return x, p
 
 def decoder_block(input, skip_features, num_filters):
     x = keras.layers.UpSampling2D(size=(2, 2))(input)
     x = keras.layers.Concatenate()([x, skip_features])
+    x = keras.layers.Dropout(DROPOUT_VALUE)(x)
     x = conv_block(x, num_filters)
     return x
+from tensorflow.keras.regularizers import l2
 
-# U-net architecture: 
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+
+# U-net architecture:
 inp = keras.layers.Input(shape=(128, 128, 1))
-s1,p1 = encoder_block(inp, 8)
-BatchNormalization()
-s2,p2 = encoder_block(p1, 16)
-BatchNormalization()
-s3,p3 = encoder_block(p2, 32)
-BatchNormalization()
-b1 = conv_block(p3, 64)
-BatchNormalization()
-d1 = decoder_block(b1, s3, 32)
-BatchNormalization()
-d2 = decoder_block(d1, s2, 16)
-BatchNormalization()
-d3 = decoder_block(d2, s1, 8)
-BatchNormalization()
-output = keras.layers.Conv2D(1, kernel_size=3, padding="same", activation='relu')(d3)
+s1,p1 = encoder_block(inp, 16)
+s2,p2 = encoder_block(p1, 32)
+s3,p3 = encoder_block(p2, 64)
+b1 = conv_block(p3, 128)
+d1 = decoder_block(b1, s3, 64)
+d2 = decoder_block(d1, s2, 32)
+d3 = decoder_block(d2, s1, 16)
+output = keras.layers.Conv2D(1, kernel_size=5, padding="same", activation='relu')(d3)
 
+early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=PATIENCE_VALUE)
+new_model = keras.models.Model(inputs= inp, outputs=output)
+new_model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(learning_rate=LR_VALUE))
+lr_scheduler = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=LR_SCH_PATIENCE_VALUE)
 
-# We load the trained model and all the weights into memory and we prediction
-callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=10)
-model = keras.models.Model(inputs= inp, outputs=output) 
-model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(learning_rate=0.001))
-print(model.summary())
-
-##########################
-model.fit(train_input,train_target, validation_data=(validation_input,validation_target),epochs=50, steps_per_epoch=20)
-
-model.save('/content/gdrive/MyDrive/Python/mojt_unet/model.keras')
+# Train the model:
+print(new_model.summary())
+new_model.fit(train_images,train_true, validation_data=(valid_images,valid_true),epochs=75, batch_size=BATCH_SIZE_VALUE, callbacks=[early_stop, lr_scheduler])
+model.save('PATH_TO_SAVE/model.keras')
 
